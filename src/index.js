db252043ce7cf1d443659884bf5a6669b591d77e
viacheslavkrupnov <slava86krupnov@gmail.com>
2021-08-16 15:05:57 +0400

    connected with redux

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ import ReactDOM from 'react-dom';
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+// import { PersistGate } from 'redux-persist/integration/react';
+import store from './redux/store';
+
+// console.log(store);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <App />
+      {/* </PersistGate> */}
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
