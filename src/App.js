e5144823da4a9672082945d81fb8162691843510
viacheslavkrupnov <slava86krupnov@gmail.com>
2021-08-16 15:06:59 +0400

    connected components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { ToastContainer } from 'react-toastify';
+import ContactForm from './components/ContactForm';
+import ContactsList from './components/ContactsList';
+import Filter from './components/Filter';
+import Container from './components/Container';
+import Section from './components/Section';
+
 function App() {
-  return <h1>Starting</h1>;
+  return (
+    <Container>
+      <Section title="Phonebook">
+        <ContactForm />
+      </Section>
+      <Section title="Contacts">
+        <Filter />
+        <ContactsList />
+        <ToastContainer autoClose={3000} />
+      </Section>
+    </Container>
+  );
 }
 
 export default App;
